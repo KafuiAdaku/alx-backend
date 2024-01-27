5cdaf8d400f07271bf07a2b0d89babb8278e07c9
Dennis Adaku <dennisadaku@ymail.com>
2024-01-27 22:47:39 +0000

    added solution to task 2

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+"""Module contaning class to paginate a database"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """
+    Args:
+        page (int):
+        page_size (int):
+
+    Returns:
+        tuple: Size 2. start index and end index
+    """
+    end_index = int(page) * int(page_size)
+    start_index = end_index - int(page_size)
+
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Args:
+            page (int): page to be displayed
+            page_size (int): number of rows displayed per page
+
+        Returns;
+            list: Rows of items of the specified page
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+
+        page = index_range(page, page_size)
+
+        csv_list = self.dataset()
+
+        upper_bound = len(csv_list)
+
+        if (not (page[0] >= 0 and page[0] <= upper_bound)) or \
+                (page[1] > upper_bound):
+            return []
+
+        return [csv_list[idx] for idx in range(page[0], page[1])]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+
+        """
+        Args:
+            page (int): page to be displayed
+            page_size (int): number of rows displayed per page
+
+        Returns;
+            dict: Containing hypermedia
+        """
+        data = self.get_page(page, page_size)
+        total_pages = - (len(self.__dataset) // - page_size)  # ceil division
+        next_page = page + 1
+        next_page = next_page if next_page <= len(self.__dataset) and \
+            next_page <= total_pages else None
+        prev_page = page - 1
+        prev_page = prev_page if prev_page >= 1 else None
+
+        return {
+                "page_size": page_size,
+                "page": page,
+                "data": data,
+                "next_page": next_page,
+                "prev_page": prev_page,
+                "total_pages": total_pages
+                }
