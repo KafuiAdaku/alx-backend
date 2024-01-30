88c035f8e7d0b363a273b11b44a63d9c32869bf3
Dennis Adaku <dennisadaku@ymail.com>
2024-01-30 22:42:50 +0000

    added solution to task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""LIFOCache module"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFOCache class implements a FIFO caching algorithm"""
+    def __init__(self):
+        """Initialise"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                del self.cache_data[key]
+                self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
+                self.MAX_ITEMS += 1
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                # delete last item before the new key was set
+                del_key = list(self.cache_data)[-2]
+                self.cache_data.pop(del_key, None)
+                print(f"DISCARD: {del_key}")
+                self.MAX_ITEMS -= 1
+
+    def get(self, key):
+        """ Get an item by key"""
+        return self.cache_data.get(key)
