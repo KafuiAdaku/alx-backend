4125eb534cafa4f78207a31c5c207672f3829a3d
Dennis Adaku <dennisadaku@ymail.com>
2024-02-07 08:45:02 +0000

    added solution to task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""main flask app  module"""
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """index page"""
+    return render_template("1-index.html")
+
+
+@babel.localeselector
+def get_local():
+    """Gets local language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(port="8080", debug=True)
