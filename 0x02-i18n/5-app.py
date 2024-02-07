49a2f4498dbc310335264eb208857224a7183aff
Dennis Adaku <dennisadaku@ymail.com>
2024-02-07 10:40:34 +0000

    added solution to task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """main flask app  module - forced local with url parameter"""
-from flask import Flask, request
+from flask import Flask, request, g
 from flask import render_template
 from flask_babel import Babel, _
 
@@ -18,6 +18,14 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
 @babel.localeselector
 def get_locale():
     """Gets local language
@@ -30,13 +38,36 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+def get_user():
+    """
+    User login mock.
+    Gets a user from a dictionary
+
+    returns:
+        int: user id
+    """
+    user_id = request.args.get("login_as")
+    if user_id and int(user_id) in users:
+        return users[int(user_id)]
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    Stores the requested user in the global varialble g
+    """
+    user = get_user()
+    g.user = user
+
+
 @app.route("/")
 def index():
     """index page route
         Returns:
             _summary_
     """
-    return render_template("4-index.html")
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
