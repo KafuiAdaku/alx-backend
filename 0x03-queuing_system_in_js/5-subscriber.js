60540549c03b6205ed7d1029ddbe5ddcb7cb84fe
Dennis Adaku <117731916+KafuiAdaku@users.noreply.github.com>
2024-03-05 22:02:56 +0000

    added solution to task 5

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,20 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client
+    .on('connect', (err) => {
+        console.log('Redis client connected to the server');
+    })
+    .on('error', (err) => {
+        console.log(`Redis client not connected to the server: ${err.message}`);
+    });
+
+client.subscribe('holberton school channel')
+client.on('message', (channel, message) => {
+        console.log(message);
+        if (message === 'KILL_SERVER') {
+            client.unsubscribe();
+            client.quit();
+        };
+});
