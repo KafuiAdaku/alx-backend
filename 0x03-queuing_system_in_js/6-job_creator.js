92e8fa89222feed1ef0149ab09c06ec971949e84
Dennis Adaku <117731916+KafuiAdaku@users.noreply.github.com>
2024-03-05 22:58:37 +0000

    added solution to task 6

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,22 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+    phoneNumber: '5551234',
+    message: 'This is a test message'
+}
+
+const job = queue.create('push_notification_code',jobData).save((err) => {
+    if (err) {
+        console.log('Notification job failed');
+    }
+    console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => {
+    console.log('Notification job completed');
+});
+job.on('failed', () => {
+    console.log('Notification job failed');
+});
